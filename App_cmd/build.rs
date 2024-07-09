148b64fca53e1362bd69484d59f84f5fc6c17f69
Xiaomony <117056212+Xiaomony@users.noreply.github.com>
2024-07-09 18:36:08 +0800

    更改了项目结构 由原本的一个package下多个crate改为了一个workspace下多个package，以适应tauri GUI框架的开发

---
+++ App_cmd/build.rs
@@ -0,0 +1,11 @@
+extern crate winres;
+
+fn main()
+{
+
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("./Giga Holy Bomb.ico");
+        res.compile().unwrap();
+    }
+}
