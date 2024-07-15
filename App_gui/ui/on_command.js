d2a1d278d5580b3b7af42d77fe98c569448905a0
Xiaomony <117056212+Xiaomony@users.noreply.github.com>
2024-07-15 18:11:28 +0800

    gui开发完成 进入测试阶段 安装包语言改为中文

--- App_gui/ui/on_command.js
+++ App_gui/ui/on_command.js
@@ -4,7 +4,20 @@ function run_command(command) {
 }
 
 function on_modArch() {
-
+    let list = get_checked();
+    if (list.length!=1) {
+        opr_addlog_warn("需要且仅能选中一个存档进行修改");
+        return;
+    }
+    window.get_modarch_info.style.display = "block";
+    window.in_modarch_name.value = "";
+    window.in_modarch_note.value = "";
+}
+function confirm_modArch() {
+    let list = get_checked();
+    var command = "ma " + list[0].toString() + ` "` + window.in_modarch_name.value + `" "` + window.in_modarch_note.value + `"`;
+    run_command(command);
+    hide_infogetter();
 }
 
 function on_usage() {
@@ -19,40 +32,76 @@ function on_save() {
 
 function on_qsave() {
     run_command("qsave");
+    refresh_arch_container();
 }
 
 function on_rsave() {
-
+    let list = get_checked();
+    if (list.length!=1) {
+        opr_addlog_warn("需要且仅能选中一个存档进行覆盖");
+        return;
+    }
+    run_command("rsave " + list[0].toString());
+    refresh_arch_container();
 }
 
 function confirm_save() {
-    var command = "save \"" + window.in_save_name.value + "\" \"" + window.in_save_note.value;
+    var command = `save "` + window.in_save_name.value + `" "` + window.in_save_note.value + `"`;
     run_command(command);
     hide_infogetter();
 }
 //-----------------load------------------------
 function on_load() {
-
+    let list = get_checked();
+    if (list.length!=1) {
+        opr_addlog_warn("需要且仅能选中一个存档读取");
+        return;
+    }
+    run_command("load " + list[0].toString());
+    refresh_arch_container();
 }
 
 function on_qload() {
-
+    run_command("qload");
 }
 //-----------------del------------------------
 function on_del() {
-
+    let list = get_checked();
+    if (list.length<=0) {
+        opr_addlog_warn("未选中存档");
+        return;
+    }
+    run_command("del " + checked_tostr(list));
+    refresh_arch_container();
 }
 
 function on_qdel() {
     run_command("qdel");
+    refresh_arch_container();
 }
 //-----------------favor------------------------
 function on_favor() {
-
+    let list = get_checked();
+    if (list.length<=0) {
+        opr_addlog_warn("未选中存档");
+        return;
+    }
+    list.forEach((item) => {
+        run_command("favor " + item.toString());
+    });
+    refresh_arch_container();
 }
 
 function on_unfavor() {
-
+    let list = get_checked();
+    if (list.length<=0) {
+        opr_addlog_warn("未选中存档");
+        return;
+    }
+    list.forEach((item) => {
+        run_command("unfavor " + item.toString());
+    })
+    refresh_arch_container();
 }
 
 
@@ -61,8 +110,12 @@ function get_checked() {
     let checked = [];
     for (let i=0;i<list.length;i++) {
         if (list[i].checked) {
-            checked.push(i-1);
+            checked.push(i + 1);
         }
     }
     return checked;
+}
+
+function checked_tostr(list) {
+    return list.join(' ');
 }
